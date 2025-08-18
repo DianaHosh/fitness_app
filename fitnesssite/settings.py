44673c9ee86975ab457314a933a78d4b7040f868
DianaHosh <diana.hoshovska.shi.2023@lpnu.ua>
2025-08-18 22:46:55 +0300

    Update files

--- fitnesssite/settings.py
+++ fitnesssite/settings.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
